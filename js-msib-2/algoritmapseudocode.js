b8b23d2b74d752af9335f53f4cde3d08ce10e6c3
bagues33 <bagustofan@yahoo.com>
2024-03-19 21:29:34 +0700

    add file

---
+++ js-msib-2/algoritmapseudocode.js
@@ -0,0 +1,51 @@
+// tentukan algoritma dan pseudocode menghitung keliling persegi panjang
+
+// Algoritma
+// 1. Mulai
+// 2. Masukkan panjang persegi panjang
+// 3. Masukkan lebar persegi panjang
+// 4. Hitung keliling persegi panjang dengan rumus 2 * (panjang + lebar)
+// 5. Tampilkan hasil keliling persegi panjang
+// 6. Selesai
+
+// Pseudocode
+// STORE "panjang" with any value
+// STORE "lebar" with any value
+// CALCULATE "keliling" = 2 * (panjang + lebar)
+// DISPLAY "keliling"
+// END
+
+// tentukan algoritma dan pseudocode menghitung luas permukaan tabung
+
+// Algoritma
+// 1. Mulai
+// 2. Masukkan jari-jari tabung
+// 3. Masukkan tinggi tabung
+// 4. Hitung luas permukaan tabung dengan rumus 2 * π * r * (r + t)
+// 5. Tampilkan hasil luas permukaan tabung
+// 6. Selesai
+
+// Pseudocode
+// STORE "r" with any value
+// STORE "t" with any value
+// CALCULATE "luasPermukaan" = 2 * π * r * (r + t)
+// DISPLAY "luasPermukaan"
+// END
+
+// tentukan algoritma dan pseudocode menghitung 1/4 volume bola
+
+// Algoritma
+// 1. Mulai
+// 2. Masukkan jari-jari bola
+// 3. Hitung volume bola dengan rumus 4/3 * π * r^3
+// 4. Hitung 1/4 volume bola dengan rumus 1/4 * volume bola
+// 5. Tampilkan hasil 1/4 volume bola
+// 6. Selesai
+
+// Pseudocode
+// STORE "r" with any value
+// CALCULATE "volumeBola" = 4/3 * π * r^3
+// CALCULATE "1/4VolumeBola" = 1/4 * volumeBola
+// DISPLAY "1/4VolumeBola"
+// END
+
---
+++ js-msib-2/variabletipedata.js
@@ -0,0 +1,66 @@
+// penjumlahan
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+// pengurangan
+var d = 10;
+var e = 20;
+var f = e - d;
+console.log(f);
+
+// perkalian
+var g = 10;
+var h = 20;
+var i = g * h;
+console.log(i);
+
+// pembagian
+var j = 10;
+var k = 20;
+var l = k / j;
+console.log(l);
+
+// modulus
+var m = 10;
+var n = 20;
+var o = n % m;
+console.log(o);
+
+// increment
+var p = 10;
+p++;
+console.log(p);
+
+// decrement
+var q = 10;
+q--;
+console.log(q);
+
+// Path: js-msib-2/variabelstring.js
+var nama = "Jhon Doe";
+console.log(nama);
+
+var alamat = "Jl. Mangga No. 100";
+console.log(alamat);
+
+var kota = "Jakarta";
+console.log(kota);
+
+var kodepos = "10286";
+console.log(kodepos);
+
+var negara = "Indonesia";
+console.log(negara);
+
+// Path: js-msib-2/variabelboolean.js
+var benar = true;
+console.log(benar);
+
+var salah = false;
+console.log(salah);
+
+// Path: js-msib-2/variabelarray.js
+var buah = ["Apel", "Jeruk", "Mangga", "Durian"];
+console.log(buah);
